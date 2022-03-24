fed55ebd9098f9b86e2c00a019384d0413316ef0
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-24 10:38:10 -0400

    returns list of duplicated columns

--- R/detect_dupl_cols.R
+++ R/detect_dupl_cols.R
@@ -3,31 +3,20 @@ cat("\014")
 
 detect_dupl_cols <- function(df){
 
-  # df = df %>% data.table
-  # col_combinations = expand.grid(x = names(df), y = names(df)) %>%
-  #   dplyr::filter(x!=y) %>%
-  #   arrange(x)
-
-  # col_combinations$x
-  # col_combinations$y
-
+  duplicate_cols = NULL
   for(col_1 in df %>% select(1:last_col(1)) %>% names()){
-    print("*************************")
     for(col_2 in df %>% select(all_of(col_1):last_col()) %>% names()){
 
       if(col_1 == col_2){
         next
       }
-      paste("checking",col_1,col_2) %>% print
-
 
       if(sum(df[,col_1] != df[,col_2]) ==0){
-        paste0(col_1, " & ", col_2, " are exactly equal") %>% print
-
-
+        duplicate_cols = c(duplicate_cols, col_2)
       }
     }
   }
+  return(duplicate_cols)
 }
 
 df = data.table::fread("99_junk/mtcars_dups.csv") %>%
@@ -53,6 +42,14 @@ df %>%
 
 # mtcars %>% data.table::fwrite("99_junk/mtcars_dups.csv")
 
+
+# df = df %>% data.table
+# col_combinations = expand.grid(x = names(df), y = names(df)) %>%
+#   dplyr::filter(x!=y) %>%
+#   arrange(x)
+
+# col_combinations$x
+# col_combinations$y
 
 
 
