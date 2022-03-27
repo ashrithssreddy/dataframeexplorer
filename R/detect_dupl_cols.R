ca7c6ca59df5a3a3fad34292fa1e363ca790c985
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-27 12:31:09 -0400

    Clean code
    Dropped stray comments and tests from code

--- R/detect_dupl_cols.R
+++ R/detect_dupl_cols.R
@@ -16,10 +16,7 @@
 #' detect_dupl_cols(dataset = head(mutate(mtcars, mpg_2 =  mpg)), duplicate_col = "right")
 #' }
 
-rm(list=ls())
-cat("\014")
-
-detect_dupl_cols <- function(df, duplicate_col = "right"){
+detect_dupl_cols <- function(dataset, duplicate_col = "right"){
 
   # A vector to collect names of duplicated columns
   duplicate_cols = NULL
@@ -49,43 +46,3 @@ detect_dupl_cols <- function(df, duplicate_col = "right"){
   } # col_1 for loop ends here
   return(duplicate_cols)
 } # function ends here
-
-df = data.table::fread("99_junk/mtcars_dups.csv") %>%
-  head %>%
-  setDF()
-set.seed(2022)
-df = df[,sample(names(df)[1:ncol(df)], ncol(df))]
-
-df %>%
-  detect_dupl_cols()
-
-#### Stray Codes ####
-# df = mtcars %>%
-#   dplyr::mutate(mpg_2 = mpg) %>%
-#   head
-
-# print(col_2)
-# print(col_1)
-# paste(col_1, col_2) %>% print
-# print("lol")
-# isTRUE(all.equal(df[,..col_1], df[,..col_2]))
-
-
-# mtcars %>% data.table::fwrite("99_junk/mtcars_dups.csv")
-
-
-# df = df %>% data.table
-# col_combinations = expand.grid(x = names(df), y = names(df)) %>%
-#   dplyr::filter(x!=y) %>%
-#   arrange(x)
-
-# col_combinations$x
-# col_combinations$y
-
-
-# Next steps
-# avoid checking duplicates of already detected duplicate column
-# docstrings
-# Return positions of columns instead of column names
-# comments
-
