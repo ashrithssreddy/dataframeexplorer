2de9b4ee51482688d32a6392dd6197a3c3a90130
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-27 12:29:18 -0400

    Improved documentation
    Added comments
    Rectified parameter name in docstring

--- R/detect_dupl_cols.R
+++ R/detect_dupl_cols.R
@@ -2,9 +2,9 @@
 #'
 #' It occasionally happens that 2 (or more) columns in dataframe are exactly identical.
 #' This could lead to redundant computational cost and unexpected behavior in Machine Learning methods.
-#' This function scans though all column combinations of dataframe and examines if any column combinations are exactly identical.
+#' This function scans though all column combinations of dataframe to examine if any 2 columns are exactly identical.
 #'
-#' @param df A data.frame
+#' @param dataset A data.frame
 #' @param duplicate_col If 2 columns are identical, which column should be treated as duplicate?
 #' Use "right" for right column, "left" for left.
 #' @return A vector of duplicate column names.
@@ -21,28 +21,34 @@ cat("\014")
 
 detect_dupl_cols <- function(df, duplicate_col = "right"){
 
+  # A vector to collect names of duplicated columns
   duplicate_cols = NULL
+
   for(col_1 in df %>% select(1:last_col(1)) %>% names()){
+
+    # Check col_1 is duplicated with any of the columns to its right
     for(col_2 in df %>% select(all_of(col_1):last_col()) %>% names()){
 
+      # Don't treat a column as a duplicate of itself, obviously!
       if(col_1 == col_2){
         next
       }
 
+      # If col_1 and col_2 are exactly identical, collect column name.
       if(all(df[,col_1] == df[,col_2])){
         if(duplicate_col == "right"){
           duplicate_cols = c(duplicate_cols, col_2)
         }else if(duplicate_col == "left"){
           duplicate_cols = c(duplicate_cols, col_1)
         }else{
-          message("Invalid value passed to duplicate_col parameter (pass 'left' or 'right'). Using default value")
+          message("Invalid value passed to duplicate_col parameter (use 'left' or 'right'). Using default value of 'right' for now")
           duplicate_cols = c(duplicate_cols, col_2)
         }
       }
-    }
-  }
+    } # col_2 for loop ends here
+  } # col_1 for loop ends here
   return(duplicate_cols)
-}
+} # function ends here
 
 df = data.table::fread("99_junk/mtcars_dups.csv") %>%
   head %>%
