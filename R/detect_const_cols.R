5987b2478df7239db534d15b85a12bd4914821cb
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-29 10:03:27 -0400

    Detect constant columns
    Added a template for new functions

---
+++ R/detect_const_cols.R
@@ -0,0 +1,13 @@
+#' Detect if any column of a data.frame constant values
+#'
+#' @param dataset A data.frame
+#' @return.
+#' @export
+#' @examples
+#' \dontrun{
+#' detect_const_cols(dataset = head(mutate(mtcars, mpg_2 =  999)))
+#' }
+
+detect_const_cols <- function(dataset, return_type = "col_names"){
+
+} # function ends here
