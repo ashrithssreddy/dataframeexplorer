ea20c7d52eedd5de8cccf29aa2056b00ce189557
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-29 10:20:12 -0400

    First cut

--- R/detect_const_cols.R
+++ R/detect_const_cols.R
@@ -10,4 +10,30 @@
 
 detect_const_cols <- function(dataset, return_type = "col_names"){
 
+  # A vector to collect names of const columns
+  const_colnames = which(sapply(dataset, function(x) length(unique(x)))==1) %>% names
+
+  if(return_type == "col_names"){
+    return(const_colnames)
+
+  }else if(return_type == "col_positions"){
+    return(which(names(dataset) %in% const_colnames))
+
+  }else if(return_type == "dataset"){
+    dataset %>% select(-any_of(const_colnames))
+
+  }else{
+    error("Invalid value passed for return_type. Use 'col_names', 'col_positions' or 'dataset'")
+  }
+
 } # function ends here
+
+# Tests
+# dataset = mtcars %>% mutate(mpg = 999, hp = 100) %>% head
+# dataset %>% detect_const_cols(return_type = "col_names")
+# dataset %>% detect_const_cols(return_type = "col_positions")
+# dataset %>% detect_const_cols(return_type = "dataset")
+
+# Next Steps
+# Missing values incorporate
+# Convert a matrix to a dataframe inside
