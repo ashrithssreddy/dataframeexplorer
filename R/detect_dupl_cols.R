be1cf5724ea3f74b92894bfc147eb3e89ac563bd
Ashrith Reddy <ashrithssreddy@gmail.com>
2022-03-29 10:06:54 -0400

    Completed documentation

--- R/detect_dupl_cols.R
+++ R/detect_dupl_cols.R
@@ -5,9 +5,11 @@
 #' This function scans though all column combinations of dataframe to examine if any 2 columns are exactly identical.
 #'
 #' @param dataset A data.frame
+#' @param return_type How to return detected duplicate columns
+#' Use "col_names", "col_positions" or "dataset" to return dataset with deleted duplicate columns
 #' @param duplicate_col If 2 columns are identical, which column should be treated as duplicate?
 #' Use "right" for right column, "left" for left.
-#' @return A vector of duplicate column names.
+#' @return A vector of duplicate column names or column positions or dataset with deleted duplicate columns. Use return_type parameter to specify.
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom dplyr select all_of last_col
